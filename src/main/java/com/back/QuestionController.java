598d6c0b6b542e9362dd669203f879febf5b14f2
hyhdb <kkminwoo@gmail.com>
2026-03-03 12:36:28 +0900

    23-2

--- src/main/java/com/back/QuestionController.java
+++ src/main/java/com/back/QuestionController.java
@@ -3,9 +3,6 @@ package com.back;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,26 +11,7 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    @ResponseBody
     public String list() {
-
-//        List<Question> questionList = questionRepository.findAll();
-//        return questionList;
-
-        String questions = questionRepository.findAll()
-                .stream()
-                .map(e -> "<li>%s</li>".formatted(e.getSubject()))
-                .collect(Collectors.joining("\n"));
-
-
-        return """
-                <html>
-                    <body>
-                        <ul>
-                          %s
-                        </ul>
-                    </body>
-                </html>
-                """.formatted(questions);
+        return "question_list";
     }
 }
