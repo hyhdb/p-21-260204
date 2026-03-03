ae530e1a56ebdb665cf5ec61604c70d6621b9335
hyhdb <kkminwoo@gmail.com>
2026-03-03 14:04:08 +0900

    23-3

--- src/main/java/com/back/QuestionController.java
+++ src/main/java/com/back/QuestionController.java
@@ -2,7 +2,10 @@ package com.back;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,7 +14,13 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    public String list() {
+    public String list(Model model) {
+
+        List<Question> questionList = questionRepository.findAll();
+
+        model.addAttribute("myAge", 20);
+        model.addAttribute("questionList", questionList);
+
         return "question_list";
     }
 }
